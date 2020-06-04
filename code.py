b31aa2b1cc7c78b1210acb2773556164a4c40217
Housey847 <64242499+Housey847@users.noreply.github.com>
2020-06-04 12:36:58 +1000

    Update code.py
    Append error duns to list and put in delay.

--- code.py
+++ code.py
@@ -15,9 +15,10 @@ duns_list = list() # stores duns
 csv_columns = ['duns','companyIdentifier','businessIdentifier','nni','entityName','address','entityType','entityTypeDescription','status','statusDescription']
 csv_file = "data.csv"
 filtered_dictionary = {}
+error_duns =[]
 payload = {}
 headers = {
-        'Authorization': 'Bearer Past JWT here',
+        'Authorization': 'Bearer ',
         'Cookie': ''
         }
     
@@ -25,7 +26,7 @@ headers = {
 line_count=0
 
 
-with open('CRM Duns List.csv') as csv_file_open:
+with open('CRM Duns List 2.csv') as csv_file_open:
     csv_reader = csv.reader(csv_file_open, delimiter=',')
     for row in csv_reader:
             duns_list.append(f'{row[0]}')
@@ -33,13 +34,17 @@ with open('CRM Duns List.csv') as csv_file_open:
     print(f'Processed {line_count} lines.')
 #print(duns_list[7:10])
 #duns_list=duns_list[5000:5314]
+line_count=0
 for dun in duns_list:
     url = 'https://express.illion.com.au/api/companysearch/?country=AU&searchType=0&searchCriteria={}'.format(dun)
-
+    error_duns.append(dun)
+    line_count += 1
+    if line_count%13==0: time.sleep(3)
     try: 
         response = requests.request("GET", url, headers=headers, data = payload)
         data =json.loads(response.text.encode('utf8'))
-        data[0]['address']=data[0]['address']['postCode']
+        if "address" in data[0]:
+            data[0]['address']=data[0]['address']['postCode']
         responses.append(data[0])
         print(dun)
 #        for key, value in data[0].items():
@@ -47,10 +52,14 @@ for dun in duns_list:
 
     except KeyError:
         print(dun + " KeyError")
+        error_duns.append(dun)
     except ConnectionError:
         print(dun + " ConnectionError error")
-    except:
+        error_duns.append(dun)
+    except Exception as e:
+        print(e)
         print(dun + " Other")
+
 
 print(responses)
 
@@ -60,9 +69,10 @@ try:
     with open(csv_file, 'w',newline='\n') as csvfile:
         writer = csv.DictWriter(csvfile,fieldnames= csv_columns)
         writer.writeheader()
-        for data in responses:
+        for data in error_duns:
             writer.writerow(data)
 except IOError:
     print("I/O error")
+    
     
 
